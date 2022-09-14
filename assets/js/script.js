40b75751ca89d6f2e42eebd336191e3220e0320a
unknown <gabemarredondo@gmail.com>
2022-09-14 16:06:27 -0700

    added a function to load saved todos on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,17 @@ $("#currentDay").text("Today is " + moment().format("dddd, MMMM Do"));
 // keeping the save button in a vareable
 let saveBtn = $(".save-btn");
 
+function loadTodo() {
+    $('.hour').each(function() {  //*goes through each .hour and check to see if there is existing data in local storage
+        let todoHour = $(this).text();
+        let previousTodo = localStorage.getItem(todoHour);
+        if (todoHour !== null) {
+            $(this).siblings('.time-block').val(previousTodo);
+        }
+    })
+};
+loadTodo();
+
 // when a save button is clicked it finds which one and saves it to local storage
 saveBtn.on("click", function () {
   let hour = $(this).siblings(".hour").text();
